fcd14f11df9dbdf8e4bd6c79a19a22113596f250
suika1 <vanyoktank1@mail.ru>
2019-06-07 00:53:40 +0300

    format birthdate, make authUser resolver

--- src/services/user/resolvers.js
+++ src/services/user/resolvers.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+import crypto from 'crypto';
 import {oncePerServices} from '../../common/services/index';
 
 export default oncePerServices((services) => {
@@ -52,14 +54,60 @@ export default oncePerServices((services) => {
         email: obj.email,
         isManager: !!obj.manager,
         isBlocked: !!obj.blocked,
-        birthdate: obj.data && obj.data.birthday || '',
+        birthdate: obj.data && moment(obj.data.birthday).format('DD.MM.YYYY') || '',
       });
 
       return rows.map(DbToGqlMapping);
     }
   }
 
+  const authUser = (builderContext) => {
+    return async (obj, args, context) => {
+      try {
+        const {
+          login,
+          password,
+        } = args;
+
+        if (Object.keys(args).length !== 2) {
+          throw new Error(`Must provide login and password`);
+        }
+
+        const statement = `SELECT
+          login,
+          password_hash
+        FROM users
+        WHERE login='${login}' `;
+
+        const { rows } = await postgres.exec({ statement });
+
+        if (!rows.length) {
+          throw new Error(`User with login '${login}' not found`);
+        }
+
+        const foundUser = rows[0];
+        // create new hash & compare it with hash from DB
+        const argHash = crypto.createHash('md5').update(password).digest('hex');
+        if (argHash !== foundUser.password_hash) {
+          throw new Error('Wrong password');
+        }
+
+        return {
+          ok: true,
+          error: '',
+        };
+      } catch (err) {
+        return {
+          ok: false,
+          error: err.message,
+        };
+      }
+
+    }
+  }
+
   return {
     userList,
+    authUser,
   }
 });
